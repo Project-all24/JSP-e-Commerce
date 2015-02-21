8aaee0cc3b0528b9329d604552ed2e7e2af35345
nacholozano <nacholozanogui@gmail.com>
2015-02-21 19:17:58 +0100

    Shopping cart has been improved

--- web/appJS/carrito.js
+++ web/appJS/carrito.js
@@ -3,12 +3,25 @@ $(document).ready(function(){
     $(document).on('click','#añadir',function(){
         
         var producto = this.name;
-        var nombre = $('.nombre[name|="'+producto+'"]').html();
-        var cantidad = $('input[name|="'+producto+'"]').val();
-        var precio = parseInt($('.precio[name|="'+producto+'"]').html());
-        var total = parseInt($('#total').html());
+        if( $('.ProCar[name|="'+producto+'"]').length > 0 ){
+            
+            var cantidad = parseInt($('.cantProCar[name|="'+producto+'"]').html());
+            var precio = parseInt($('.precioProCar[name|="'+producto+'"]').html());
+            var cantidadPlus = parseInt($('input[name|="'+producto+'"]').val());
+            $('.cantProCar[name|="'+producto+'"]').html(cantidad+cantidadPlus);
         
-        $('.productsCar').append('<div class="ProCar" name="'+nombre+'">'
+            var total = parseInt($('#total').html()); 
+            
+            $('#total').html( total + (precio*cantidadPlus) );
+        
+        }else{
+            
+            var nombre = $('.nombre[name|="'+producto+'"]').html();
+            var cantidad = parseInt($('input[name|="'+producto+'"]').val());
+            var precio = parseInt($('.precio[name|="'+producto+'"]').html());
+            var total = parseInt($('#total').html());
+
+            $('.productsCar').append('<div class="ProCar" name="'+nombre+'">'
                 +'<div class="nombreProCar">'
                 +nombre
                 +'</div>'
@@ -21,9 +34,10 @@ $(document).ready(function(){
                 +'</div>'
                 +'<button id="borrarProduc" name="'+nombre+'" class="glyphicon glyphicon-remove"></button>'
                 +'</div>');
+
+            $('#total').html(total + (precio*cantidad));
         
-        $('#total').html(total + (precio*cantidad));
-        
+        }
     });
     
     $(document).on('click','#borrarProduc',function(){
